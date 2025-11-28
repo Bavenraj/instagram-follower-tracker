92402ae5d8afee4a25d61ef27458c64b13c4bd16
Bavenraj <bavenraj08@gmail.com>
2025-11-28 22:53:01 +0800

    Wrapped into class

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from insta_crawler import Crawler
+from insta_scraper import Scraper
+from dotenv import load_dotenv
+
+load_dotenv()
+Crawler = Crawler(user_id= os.getenv("user_id"), password="your_password")
+
+Crawler.login()
+Crawler.scrape_followers()
+Crawler.scrape_following()
+Crawler.close()
